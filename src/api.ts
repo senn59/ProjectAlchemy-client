f434423c4aa0ed41a18392af4c6ef15f4b9cc19e
senn59 <74777070+senn59@users.noreply.github.com>
2024-11-23 20:01:57 +0100

    chore: remove unnecessary print

--- src/api.ts
+++ src/api.ts
@@ -7,7 +7,6 @@ const api = axios.create({
 api.defaults.headers.common["Content-Type"] = "application/json";
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem("auth-jwt");
-    console.log(token);
     if (token) {
         config.headers.Authorization = `Bearer ${token}`;
     }
