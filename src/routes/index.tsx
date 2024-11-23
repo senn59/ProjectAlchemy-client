787bc9ea0f6cc3b21bda86b484be21e17c69c20b
senn59 <74777070+senn59@users.noreply.github.com>
2024-11-23 20:01:18 +0100

    feat: sign in btn / logo in home page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import { ModeToggle } from "@/components/shadcn/theme-toggle.tsx";
 import { Button } from "@/components/ui/button.tsx";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import { useAuth } from "@/auth/authprovider.tsx";
 
@@ -16,12 +16,15 @@ export default function Index() {
 
     return (
         <>
+            <div className="absolute left-8 top-4">
+                <h1 className="font-bold text-xl">ProjectAlchemy</h1>
+            </div>
             <div className="absolute right-8 top-4">
                 <ModeToggle />
             </div>
             <div className="h-screen w-screen flex flex-col items-center justify-center">
                 <div className="w-1/3 flex flex-col items-center justify-center">
-                    <h1 className="scroll-m-20 text-4xl font-extrabold text-center tracking-tight lg:text-5xl">
+                    <h1 className="text-4xl font-extrabold text-center tracking-tight lg:text-5xl">
                         Lightweight Agile Project Management Tool
                     </h1>
                     <div className="py-3"></div>
@@ -31,13 +34,19 @@ export default function Index() {
                     </p>
                     <div className="py-3"></div>
                     <div>
-                        <Button
-                            variant="outline"
-                            onClick={() => navigate("/auth/signup")}
-                        >
-                            Sign up
+                        <Button variant="outline">
+                            <Link to="/auth/signup">Sign up</Link>
                         </Button>
                     </div>
+                    <p className="text-muted-foreground py-3 text-center">
+                        Already have an account?{" "}
+                        <Link
+                            to="/auth/signin"
+                            className="font-bold hover:underline"
+                        >
+                            Sign in
+                        </Link>
+                    </p>
                 </div>
             </div>
         </>
