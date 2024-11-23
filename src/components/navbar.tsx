8a36d58ce68b4971b5b479f541abb55c359e8286
senn59 <74777070+senn59@users.noreply.github.com>
2024-11-23 12:57:31 +0100

    feat: add logout button

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Separator } from "@/components/ui/separator";
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "@/components/shadcn/theme-toggle";
+import { useAuthStore } from "@/auth/store.ts";
 
 function Navbar() {
+    const store = useAuthStore();
+    const navigate = useNavigate();
+
+    const signOut = () => {
+        store.logout();
+        navigate("/");
+    };
     return (
         <>
             <div className="w-screen h-16 flex items-center justify-between">
@@ -13,10 +21,12 @@ function Navbar() {
                         <Link to="/board">Board</Link>
                     </Button>
                 </div>
-                <div className="pr-5 flex items-center justify-between w-40">
-                    <ModeToggle />
-                    <Button variant={"outline"}>
-                        <Link to="/login">Log In</Link>
+                <div className="pr-5 flex">
+                    <div className="mr-4">
+                        <ModeToggle />
+                    </div>
+                    <Button variant={"outline"} onClick={signOut}>
+                        Sign out
                     </Button>
                 </div>
             </div>
