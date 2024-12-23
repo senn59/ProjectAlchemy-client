64a90145f1d4444da2f6ec50ae16fa652a2e6857
senn59 <74777070+senn59@users.noreply.github.com>
2024-12-23 23:05:14 +0100

    chore: small changes

--- src/issues/columns.tsx
+++ src/issues/columns.tsx
@@ -6,19 +6,20 @@ export const columns: ColumnDef<PartialIssue>[] = [
     {
         accessorKey: "id",
         header: "Id",
-        size: 0,
+        size: 5,
     },
     {
         accessorKey: "type",
         header: "Type",
-        size: 0,
+        size: 5,
         cell: ({ row }) => (
             <IssueTypeSelect issueId={row.original.id} silent={false} />
         ),
     },
     {
         accessorKey: "name",
-        header: "Name",
-        size: 200,
+        header: () => <div className="pl-8">Name</div>,
+        size: 0,
+        cell: ({ row }) => <div className="pl-8">{row.getValue("name")}</div>,
     },
 ];
