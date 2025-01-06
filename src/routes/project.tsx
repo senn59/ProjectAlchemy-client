fb8748f70cafa2c8e5ff82ee56d550e0a52ff3f9
senn59 <74777070+senn59@users.noreply.github.com>
2025-01-06 08:21:44 +0100

    feat: add members popover for non owner members

--- src/routes/project.tsx
+++ src/routes/project.tsx
@@ -2,10 +2,10 @@ import { Link, useParams } from "react-router-dom";
 import { useContext } from "react";
 import { IssuesTable } from "@/issues/issues-table.tsx";
 import { Button } from "@/components/ui/button.tsx";
-import { LucideSettings } from "lucide-react";
 import { ProjectContext } from "@/projects/project-provider.tsx";
 import { useAuth } from "@/auth/authprovider.tsx";
 import { memberType } from "@/projects/types.ts";
+import { MembersPopover } from "@/projects/members-popover.tsx";
 
 export default function Project() {
     const { project } = useContext(ProjectContext);
@@ -22,12 +22,12 @@ export default function Project() {
                                 {project.name}
                             </h1>
                             {project.members.find((m) => m.userId === user?.id)
-                                ?.type === memberType.Owner && (
+                                ?.type === memberType.Owner ? (
                                 <Link to={`/projects/${id}/settings`}>
-                                    <Button size="icon" variant="ghost">
-                                        <LucideSettings size={20} />
-                                    </Button>
+                                    <Button>Go to settings</Button>
                                 </Link>
+                            ) : (
+                                <MembersPopover members={project.members} />
                             )}
                         </div>
                         <div className="my-12">
